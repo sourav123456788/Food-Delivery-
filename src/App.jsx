67c75c0d78b25792aac7e89f1d6588e260761350
sourav dey <deys9588@gmail.com>
2025-04-16 14:24:35 +0530

    Fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import Navbar from './components/Navbar/Navbar'
+import Navbar from './components/navbar/Navbar'
 import { Route, Routes } from 'react-router-dom'
 import PlaceOrder from './pages/placeOrder/placeOrder'
 import Home from './pages/home/home'
